d8c4b65405a94fc35dc3a5565d0766ca0581d645
Elliott-Wong02 <ekw120@ic.ac.uk>
2022-10-26 01:55:57 +0100

    v1.2

--- src/main/java/Drawing.java
+++ src/main/java/Drawing.java
@@ -1,3 +1,6 @@
+import Shapes.Circle;
+import Shapes.Rect;
+
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -6,7 +9,7 @@ public class Drawing extends Canvas{
     Color c = new Color(0x992266);
     Point p_2 = new Point(0, 0);
     Color c_2 = new Color(49,27,59);
-    private Circle O = new Circle(p, c, 200);
+    private Circle O = new Circle(p, c, 100);
     private Rect R = new Rect(p_2, c_2, 100, 200);
     private Frame f;
     public Drawing(){
